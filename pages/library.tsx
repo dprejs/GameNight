451938367daaa658311f42ffe760453513d23351
Daniel Prejs <danielnprejs@gmail.com>
2022-02-26 11:54:20 -0700

    add game modal added

--- pages/library.tsx
+++ pages/library.tsx
@@ -1,16 +1,51 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
 
-const library:FC = (props) => {
+const modalBoxStyle = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
+const Library: FC = (props) => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return (
     <>
-    <h1>
-      library
-    </h1>
-    <button>Add Game +</button>
+      <div>
+        <h1>
+          library
+        </h1>
+        <button onClick={handleOpen}>Add Game +</button>
+        <Modal
+          open={open}
+          onClose={handleClose}
+        >
+          <Box sx={modalBoxStyle}>
+            <div>
+              <h2>
+              Search for Game
+              </h2>
+              <input type="text"></input>
+              <SearchRoundedIcon/>
+            </div>
+          </Box>
+        </Modal>
+      </div>
+
     </>
   )
 
 }
 
-export default library;
+
+export default Library;
