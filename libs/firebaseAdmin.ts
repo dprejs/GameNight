6d5bcac37b1f9f6e80309598ce1c192081ccde74
Daniel Prejs <danielnprejs@gmail.com>
2024-11-19 15:43:00 -0700

    allow firebase key auth in env

--- libs/firebaseAdmin.ts
+++ libs/firebaseAdmin.ts
@@ -1,11 +1,17 @@
 import admin, { initializeApp } from 'firebase-admin';
-const serviceAccount = require('../game-night-2dfb7-firebase-adminsdk-p9y0e-8d9325ad7f.json');
 import * as firebase from 'firebase-admin/app';
 var app;
 if (!firebase.getApps().length) {
-  app = admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  })
+  if (process.env.SERVICE_ACCOUNT) {
+    app = admin.initializeApp({
+      credential: admin.credential.cert(process.env.SERVICE_ACCOUNT),
+    })
+  } else {
+    const serviceAccount = require('../game-night-2dfb7-firebase-adminsdk-p9y0e-8d9325ad7f.json');
+    app = admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    })
+  }
 
 }else {
   app = firebase.getApp()
