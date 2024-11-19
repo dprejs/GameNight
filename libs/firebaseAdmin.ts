e121bfff68e57872091ba9c12aa3bf9c13f183cb
Daniel Prejs <danielnprejs@gmail.com>
2024-11-19 16:07:03 -0700

    removed reference to service account file

--- libs/firebaseAdmin.ts
+++ libs/firebaseAdmin.ts
@@ -3,18 +3,18 @@ import * as firebase from 'firebase-admin/app';
 import fs from "fs"
 var app;
 if (!firebase.getApps().length) {
-  if (process.env.SERVICE_ACCOUNT) {
+  // if (process.env.SERVICE_ACCOUNT) {
     app = admin.initializeApp({
-      credential: admin.credential.cert(process.env.SERVICE_ACCOUNT),
+      credential: admin.credential.cert(JSON.parse(process.env.SERVICE_ACCOUNT)),
     })
-  } else {
-    if (fs.existsSync('../game-night-2dfb7-firebase-adminsdk-p9y0e-8d9325ad7f.json')){
-      const serviceAccount = require('../game-night-2dfb7-firebase-adminsdk-p9y0e-8d9325ad7f.json');
-      app = admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
-      })
-    }
-  }
+  // } else {
+    // if (fs.existsSync('../game-night-2dfb7-firebase-adminsdk-p9y0e-8d9325ad7f.json')){
+    //   const serviceAccount = require('../game-night-2dfb7-firebase-adminsdk-p9y0e-8d9325ad7f.json');
+    //   app = admin.initializeApp({
+    //     credential: admin.credential.cert(serviceAccount),
+    //   })
+    // }
+  // }
 
 }else {
   app = firebase.getApp()
